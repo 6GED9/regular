8fa1d3c5cf30e1816dc48f8d83aa57f763bde74f
wudisluv <vlad.kornilov2015@gmail.com>
2022-09-27 12:57:18 +0300

    regular

---
+++ regular/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Regular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Завтрак в 23:60";
+            Regex regex = new Regex("[0-1]{1}[0-9]{1}:[0-5]{1}[0-9]{1}");
+            MatchCollection matches = regex.Matches(s);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+            else
+            {
+                regex = new Regex("[2]{1}[0-3]{1}:[0-5]{1}[0-9]{1}");
+                matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    foreach (Match match in matches)
+                        Console.WriteLine(match.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Неправильное время");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
